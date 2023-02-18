cffb0ab046ce001693d97520f7fa85ece00eaa0c
Samvel <samvel121202@mail.ru>
2023-02-18 23:59:24 +0300

    Added button and input components

---
+++ src/components/buttons/FilledButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button :class="color">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+  color?: "primary" | "primary-light" | "secondary";
+}
+
+withDefaults(defineProps<Props>(), {
+  color: "primary",
+});
+</script>
+
+<style lang="scss" scoped>
+button {
+  cursor: pointer;
+  padding: 10px 20px;
+  height: 40px;
+  font-size: 15px;
+  border: 0;
+  margin: 0;
+  border-radius: 5px;
+  min-width: 120px;
+  &.primary {
+    background: var(--primary-color);
+    color: white;
+    &:hover {
+      background: none;
+      outline: 1px solid var(--primary-color);
+      color: var(--primary-color);
+    }
+  }
+  &.primary-light {
+    background: var(--primary-light-color);
+    color: var(--text-color);
+    &:hover {
+      background: none;
+      outline: 1px solid var(--primary-light-color);
+    }
+  }
+  &.secondary {
+    background: var(--secondary-color);
+    color: white;
+    &:hover {
+      background: none;
+      outline: 1px solid var(--secondary-color);
+      color: var(--secondary-color);
+    }
+  }
+}
+</style>
---
+++ src/components/inputs/BorderedInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <input :class="color" />
+</template>
+
+<script setup lang="ts">
+export interface Props {
+  color?: "gray" | "primary";
+}
+
+withDefaults(defineProps<Props>(), {
+  color: "gray",
+});
+</script>
+
+<style lang="scss" scoped>
+input {
+  width: 100%;
+  padding: 0 20px;
+  height: 40px;
+  background: transparent;
+  outline: none;
+  border-radius: 3px;
+  font-size: 14px;
+  &.gray {
+    color: var(--text-color);
+    border: 1px solid var(--input-color);
+    &:focus {
+      border: 2px solid var(--input-color);
+      padding: 0 19px;
+    }
+    &::placeholder {
+      color: var(--secondary-color);
+    }
+  }
+  &.primary {
+    color: var(--text-color);
+    border: 1px solid var(--primary-color);
+    &:focus {
+      border: 2px solid var(--primary-color);
+      padding: 0 19px;
+    }
+    &::placeholder {
+      color: var(--secondary-color);
+    }
+  }
+}
+</style>
