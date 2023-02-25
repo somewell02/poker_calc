d3c0a0e3a6ac138271b543de26aac4bb61052747
Samvel <samvel121202@mail.ru>
2023-02-25 17:55:49 +0300

    Added mask option on input

--- src/components/inputs/TextInput.vue
+++ src/components/inputs/TextInput.vue
@@ -1,18 +1,32 @@
 <template>
-  <bordered-input type="text" :value="modelValue ?? ''" @input="updateInput" />
+  <bordered-input
+    type="text"
+    v-maska
+    :data-maska="mask"
+    :data-maska-tokens="maskTokens"
+    :color="color"
+    :value="modelValue ?? ''"
+    @input="updateInput"
+  />
 </template>
 
 <script setup lang="ts">
 import BorderedInput from "./BorderedInput";
+
 import { inputColors } from "@/types/colors";
+import { vMaska } from "maska";
 
 export interface propsInterface {
   modelValue: string;
   color?: inputColors;
+  mask?: string;
+  maskTokens?: string;
 }
 
 withDefaults(defineProps<propsInterface>(), {
   color: "gray",
+  mask: "",
+  maskTokens: "@:[а-яА-Я,a-zA-Z]",
 });
 
 const emits = defineEmits(["update:modelValue"]);
